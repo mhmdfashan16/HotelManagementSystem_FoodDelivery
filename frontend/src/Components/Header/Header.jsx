da1f40009e2cc6b84b869bfc13cdd820791c25c1
mhmdfashan16 <mhmdfshn16@gmail.com>
2025-05-08 09:41:24 +0530

    header.jsx and css files updated

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header_content">
-            <h2>Order Your Favorite food Here</h2>
+            <h2>Delivering your favorite feasts</h2>
             <p>Choose from a diverse menu featuring a deletable array of dishes crafted with the finest ingredience and culinary expetrtice Our missing is to satisfy your cravings and elevate your dinig experience, one delicious meal at a time.</p>
             <a href="#explore_menu">
             <button >View Menu</button>
