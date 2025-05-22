673d614094fd4ac931f4248e9d589d88760955fb
mhmdfashan16 <mhmdfshn16@gmail.com>
2025-05-22 20:32:59 +0530

    project assets added

--- frontend/src/Components/Appdownload/Appdownload.jsx
+++ frontend/src/Components/Appdownload/Appdownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../Assets/assets'
 const Appdownload = () => {
   return (
     <div className='app_download' id='app_download'>
-      <p>For Better Experience Download <br/>Tomoto App</p>
+      <p>For Better Experience Download <br/>Feastify App</p>
       <div className="app_download_plateform">
         <img src={assets.play_store}/>
         <img src={assets.app_store}/>
